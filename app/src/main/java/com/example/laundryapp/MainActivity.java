f1f537f923fef7e336630a831d63c9f3ee4ca826
Andrei Stoyanow <stoyanowaa@hendrix.edu>
2019-01-30 09:29:38 -0600

    Added Buttons and Text View
    Added buttons for washer/dryer times and options and change font size and style for title.

--- app/src/main/java/com/example/laundryapp/MainActivity.java
+++ app/src/main/java/com/example/laundryapp/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.laundryapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button washerStart;
+    private Button dryerStart;
+    private Button optionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        washerStart = findViewById(R.id.startWasher);
+        dryerStart = findViewById(R.id.startDryer);
+        optionButton = findViewById(R.id.optionsButton);
     }
 }
